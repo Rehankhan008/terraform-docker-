f8e90f897eb035e3d57d25a2deaa907c5167bbab
Rehan khan <143535840+Rehankhan008@users.noreply.github.com>
2025-01-08 16:14:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,22 +2,23 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "2.16.0"
+      version = "~> 2.0"
     }
   }
 }
 
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
+# Only define the provider block once
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
 }
 
-# Pulls the image
-resource "docker_image" "ubuntu" {
-  name = "ubuntu:latest"
-}
-
-# Create a container
-resource "docker_container" "foo" {
-  image = docker_image.ubuntu.latest
-  name  = "foo"
+resource "docker_container" "nginx" {
+  name  = "nginx-container"
+  image = docker_image.nginx.latest
+  ports {
+    internal = 80
+    external = 8080
+  }
 }
